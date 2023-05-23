aa409ac4734e23f85f70026082cdb8e7f51f6eea
Audrey Jiang <ajiang@lcs.on.ca>
2023-05-23 11:37:43 -0400

    Initial Commit

---
+++ Applemusic/ApplemusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApplemusicApp.swift
+//  Applemusic
+//
+//  Created by Audrey Jiang on 2023-05-23.
+//
+
+import SwiftUI
+
+@main
+struct ApplemusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
